d85672820b048e94810993834c63da35d2dd3d0d
gcm10000 <gabrielc.machado@hotmail.com>
2016-11-26 22:16:29 -0200

    SocketUDP

---
+++ SocketsUDP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+
+namespace App1
+{
+   class SocketsUDP
+    {
+        HostName hostname = new HostName("");
+        DatagramSocket listener = new DatagramSocket();
+
+    }
+}
